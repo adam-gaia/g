320264ccb19e59922906492202255b894c6ff9fe
Adam Gaia <git@agaia.dev>
2025-04-29 10:53:54 -0600

    feat: direnv and devshell fixes

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,16 +1,16 @@
 {
-  flake,
   inputs,
-  perSystem,
   pkgs,
 }: let
+  lib = inputs.nixpkgs.lib;
+
   # Treefmt doesn't easily expose the programs with out its flake-parts module (as far as I can tell)
   # This snipit, modified from their default.nix, lets us grab the programs after building with our treefmt config
   treefmt-module-builder = nixpkgs: configuration: let
     mod = inputs.treefmt-nix.lib.evalModule nixpkgs configuration;
   in
     mod.config.build;
-  treefmt-module = treefmt-module-builder pkgs (import ../treefmt.nix);
+  treefmt-module = treefmt-module-builder pkgs (import ./treefmt.nix);
   treefmt-bin = treefmt-module.wrapper;
   treefmt-programs = lib.attrValues treefmt-module.programs;
 
@@ -27,7 +27,6 @@ in
   pkgs.mkShellNoCC {
     packages =
       [
-
       ]
       # Include treefmt and formatters
       ++ treefmt-programs
